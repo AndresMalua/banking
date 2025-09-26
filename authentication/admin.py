e1aed9e1a31ca4939d2ec29043c33d46545a7859
Andres <maluaandres1@gmail.com>
2025-09-26 12:58:41 -0500

    NODE.JS

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from.models import Country,Department,City,User
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(Department)
+admin.site.register(City)
